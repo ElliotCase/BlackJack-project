9e80d9abced49294ce15759448d2cafbf9435ff0
ElliotCase <40835798+ElliotCase@users.noreply.github.com>
2022-12-26 22:16:01 +0000

    refactored function for calculating score to one function
    also added logic for player to draw another card if their score is under 17 after two cards

--- blackjack.js
+++ blackjack.js
@@ -7,47 +7,25 @@ let dealerHandScore = 0;
 function startGame() {
   drawOneCard(playerHand);
   drawOneCard(playerHand);
-
-  calculatePlayerHandScore();
-
-  console.log(
-    `Starting Player cards = ${playerHand} - score = ${playerHandScore}`
-  );
+  let playerScore = calculateHandScore(playerHandScore, playerHand);
+  console.log(`Starting Player cards = ${playerHand}  score = ${playerScore}`);
   drawOneCard(dealerHand);
   drawOneCard(dealerHand);
 
-  calculateDealerHandScore();
-  console.log(
-    `Starting Dealer cards = ${dealerHand} - score = ${dealerHandScore}`
-  );
+  let dealerScore = calculateHandScore(dealerHandScore, dealerHand);
+  console.log(`Starting Dealer cards = ${dealerHand}  score = ${dealerScore}`);
 }
 
 startGame();
 function drawOneCard(e) {
   for (let i = 0; i < 1; i++) {
-    const cards = [
-      "Ace",
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      "Jack",
-      "Queen",
-      "King",
-    ];
+    const cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
     const suit = ["Spades", "Hearts", "Diamonds", "Clubs"];
     let randomCard = cards[Math.floor(Math.random() * cards.length)];
 
     let randomSuit = suit[Math.floor(Math.random() * suit.length)];
 
     firstTimeCards.push(" " + randomCard + " Of " + randomSuit);
-    console.log(firstTimeCards);
     if (checkIfArrayIsUnique(firstTimeCards)) {
       e.push(" " + randomCard + " Of " + randomSuit);
     } else if (firstTimeCards.length == 3) {
@@ -63,62 +41,43 @@ function checkIfArrayIsUnique(myArray) {
 
 /////calculate score of hand
 
-function calculatePlayerHandScore() {
-  for (let i = 0; i < playerHand.length; i++) {
-    if (playerHand[i].includes("Ace")) {
-      playerHandScore += 11;
-    } else if (playerHand[i].includes("2")) {
-      playerHandScore += 2;
-    } else if (playerHand[i].includes("3")) {
-      playerHandScore += 3;
-    } else if (playerHand[i].includes("4")) {
-      playerHandScore += 4;
-    } else if (playerHand[i].includes("5")) {
-      playerHandScore += 5;
-    } else if (playerHand[i].includes("6")) {
-      playerHandScore += 6;
-    } else if (playerHand[i].includes("7")) {
-      playerHandScore += 7;
-    } else if (playerHand[i].includes("8")) {
-      playerHandScore += 8;
-    } else if (playerHand[i].includes("9")) {
-      playerHandScore += 9;
-    } else if (playerHand[i].includes("10")) {
-      playerHandScore += 10;
-    } else if (playerHand[i] == "King" || "Queen" || "Jack") {
-      playerHandScore += 10;
+function calculateHandScore(score, whosHand) {
+  for (let i = 0; i < whosHand.length; i++) {
+    if (whosHand[i].includes("Ace")) {
+      score += 11;
+    } else if (whosHand[i].includes("2")) {
+      score += 2;
+    } else if (whosHand[i].includes("3")) {
+      score += 3;
+    } else if (whosHand[i].includes("4")) {
+      score += 4;
+    } else if (whosHand[i].includes("5")) {
+      score += 5;
+    } else if (whosHand[i].includes("6")) {
+      score += 6;
+    } else if (whosHand[i].includes("7")) {
+      score += 7;
+    } else if (whosHand[i].includes("8")) {
+      score += 8;
+    } else if (whosHand[i].includes("9")) {
+      score += 9;
+    } else if (whosHand[i].includes("10")) {
+      score += 10;
+    } else if (whosHand[i] == "King" || "Queen" || "Jack") {
+      score += 10;
     }
   }
-}
-function calculateDealerHandScore() {
-  for (let i = 0; i < dealerHand.length; i++) {
-    if (dealerHand[i].includes("Ace")) {
-      dealerHandScore += 11;
-    } else if (dealerHand[i].includes("2")) {
-      dealerHandScore += 2;
-    } else if (dealerHand[i].includes("3")) {
-      dealerHandScore += 3;
-    } else if (dealerHand[i].includes("4")) {
-      dealerHandScore += 4;
-    } else if (dealerHand[i].includes("5")) {
-      dealerHandScore += 5;
-    } else if (dealerHand[i].includes("6")) {
-      dealerHandScore += 6;
-    } else if (dealerHand[i].includes("7")) {
-      dealerHandScore += 7;
-    } else if (dealerHand[i].includes("8")) {
-      dealerHandScore += 8;
-    } else if (dealerHand[i].includes("9")) {
-      dealerHandScore += 9;
-    } else if (dealerHand[i].includes("10")) {
-      dealerHandScore += 10;
-    } else if (dealerHand[i] == "King" || "Queen" || "Jack") {
-      dealerHandScore += 10;
-    }
-  }
+  return score;
 }
 
 function drawAgain() {
   if (playerHandScore < 17) {
+    console.log("drawing another card for the player");
+    drawOneCard(playerHand);
+    playerScore = calculateHandScore(playerHandScore, playerHand);
+    console.log(playerHand);
+    console.log(playerScore);
   }
 }
+
+drawAgain();
