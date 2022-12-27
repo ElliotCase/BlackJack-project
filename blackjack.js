535adaeba8b74ec5d3c442d742bfccb89cb8ec85
ElliotCase <40835798+ElliotCase@users.noreply.github.com>
2022-12-27 00:31:17 +0000

    Add files via upload

--- blackjack.js
+++ blackjack.js
@@ -3,32 +3,66 @@ let dealerHand = [];
 let firstTimeCards = [];
 let playerHandScore = 0;
 let dealerHandScore = 0;
-
+let playerScore = 0;
+let updatedScore = 0;
+let updatedScore2 = 0;
+let playerScoreUpdated = 0;
+let updatedFinalScore = 0;
 function startGame() {
   drawOneCard(playerHand);
   drawOneCard(playerHand);
-  let playerScore = calculateHandScore(playerHandScore, playerHand);
+  playerScore = calculateHandScore(playerHandScore, playerHand);
   console.log(`Starting Player cards = ${playerHand}  score = ${playerScore}`);
   drawOneCard(dealerHand);
   drawOneCard(dealerHand);
 
   let dealerScore = calculateHandScore(dealerHandScore, dealerHand);
-  console.log(`Starting Dealer cards = ${dealerHand}  score = ${dealerScore}`);
+  // console.log(`Starting Dealer cards = ${dealerHand}  score = ${dealerScore}`);
+
+  playerScoreUpdated = drawAgain(
+    playerScore,
+    playerHand,
+    playerHandScore,
+    updatedScore
+  );
+  logEndScore();
+
+  checkIfBust(playerScoreUpdated, playerHand);
+  checkIfBust(playerScoreUpdated, playerHand);
 }
 
 startGame();
 function drawOneCard(e) {
   for (let i = 0; i < 1; i++) {
-    const cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+    const cards = [
+      "Ace",
+      // 1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      "Jack",
+      "Queen",
+      "King",
+    ];
     const suit = ["Spades", "Hearts", "Diamonds", "Clubs"];
     let randomCard = cards[Math.floor(Math.random() * cards.length)];
 
     let randomSuit = suit[Math.floor(Math.random() * suit.length)];
 
     firstTimeCards.push(" " + randomCard + " Of " + randomSuit);
+    //    console.log(firstTimeCards);
     if (checkIfArrayIsUnique(firstTimeCards)) {
+      //   console.log("this is a new item in the array");
       e.push(" " + randomCard + " Of " + randomSuit);
-    } else if (firstTimeCards.length == 3) {
+    } else {
+      //    console.log("I am popping off the last item in the array which is");
+      //     console.log(firstTimeCards[firstTimeCards.length - 1]);
       firstTimeCards.pop();
       drawOneCard(e);
     }
@@ -38,7 +72,6 @@ function drawOneCard(e) {
 function checkIfArrayIsUnique(myArray) {
   return myArray.length === new Set(myArray).size;
 }
-
 /////calculate score of hand
 
 function calculateHandScore(score, whosHand) {
@@ -70,14 +103,46 @@ function calculateHandScore(score, whosHand) {
   return score;
 }
 
-function drawAgain() {
-  if (playerHandScore < 17) {
-    console.log("drawing another card for the player");
-    drawOneCard(playerHand);
-    playerScore = calculateHandScore(playerHandScore, playerHand);
-    console.log(playerHand);
-    console.log(playerScore);
+function drawAgain(score, whosHand, whosOldScore, newScore) {
+  if (score < 17) {
+    console.log("score is under 17");
+    console.log(`
+    
+drawing another card for the player`);
+    drawOneCard(whosHand);
+    newScore = calculateHandScore(whosOldScore, whosHand);
+    console.log(whosHand[whosHand.length - 1]);
+    console.log(newScore);
   }
+  return newScore;
 }
 
-drawAgain();
+function checkIfBust(e, r) {
+  if (e > 21) {
+    console.log(`
+    
+The New Score for player is ${playerScoreUpdated}`);
+
+    console.log("player is bust you lose");
+    return;
+  } else if (e < 17) {
+    console.log("drawing another card for the player");
+
+    drawOneCard(r);
+    console.log(r[r.length - 1]);
+    playerScoreUpdated = calculateHandScore(playerHandScore, playerHand);
+    console.log(playerScoreUpdated);
+    if (playerScoreUpdated < 21) {
+      console.log(`
+    
+    The New Score for player is ${playerScoreUpdated}`);
+    }
+  }
+  return;
+}
+
+function logEndScore() {
+  if (playerScoreUpdated > 17 && playerScoreUpdated < 21) {
+    console.log(`Player End Score = ${playerScoreUpdated}`);
+  }
+}
