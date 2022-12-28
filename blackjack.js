495ab1a46935ef01d2ecfec3f0aa680e9260ffb0
DESKTOP-FGDRREL\ellio <elliotcase@hotmail.co.uk>
2022-12-28 13:30:20 +0000

    refactored all functions, added comments, added logic to allow 'ACE' card to be the value of 11 or 1

--- blackjack.js
+++ blackjack.js
@@ -4,87 +4,83 @@ let firstTimeCards = [];
 let playerHandScore = 0;
 let dealerHandScore = 0;
 let playerScore = 0;
-let updatedScore = 0;
-let updatedScore2 = 0;
-let playerScoreUpdated = 0;
 let dealerScore = 0;
-let updatedFinalScore = 0;
 
+//Main function that runs the game from start to finish
 function startGame() {
+  // Draws starting two cards for the player, calculates and logs the players score and the cards they drew
   drawOneCard(playerHand);
   drawOneCard(playerHand);
-  playerScoreUpdated = calculateHandScore(playerHandScore, playerHand);
-  console.log(
-    `Starting Player cards = ${playerHand}  score = ${playerScoreUpdated}`
-  );
+  playerScore = calculateHandScore(playerHandScore, playerHand);
+  console.log(`Starting Player cards = ${playerHand}  score = ${playerScore}`);
+
+  // Draws starting two cards for the dealer, calculates and logs the dealers score and the cards they drew
   drawOneCard(dealerHand);
   drawOneCard(dealerHand);
   dealerScore = calculateHandScore(dealerHandScore, dealerHand);
   console.log(`Starting Dealer cards = ${dealerHand}  score = ${dealerScore}`);
 
-  while (playerScoreUpdated < 17) {
+  // while player is under 17 keep drawing cards one at a time and logging the card drawn and the new updated score
+  console.log("");
+  while (playerScore < 17) {
+    console.log("Drawing another card for the player");
     drawOneCard(playerHand);
-    console.log(playerHand);
-    playerScoreUpdated = calculateHandScore(playerHandScore, playerHand);
-    console.log(playerScoreUpdated);
+    playerScore = calculateHandScore(playerHandScore, playerHand);
+    console.log(`The players score is now ${playerScore}`);
   }
-  while (dealerScore < 17 && playerScoreUpdated <= 21) {
-    drawOneCard(dealerHand);
-    console.log(dealerHand);
-    dealerScore = calculateHandScore(dealerHandScore, dealerHand);
-    console.log(dealerScore);
-  }
-  logEndScore("player", playerScoreUpdated);
-  logEndScore("dealer", dealerScore);
+  // The players score is now over 17 log out the final players score
+  logEndScore("player", playerScore);
+  console.log("");
+  console.log("");
+  console.log("");
+  console.log("");
 
-  checkIfBust(playerScoreUpdated, "player");
-  checkIfBust(dealerScore, "dealer");
+  //While the dealers score is under 17 and the player did not bust, keep drawing one card at a time
+  //Logging each card and the new score
+  while (dealerScore < 17 && playerScore <= 21) {
+    console.log("Drawing another card for the Dealer");
+    drawOneCard(dealerHand);
+    dealerScore = calculateHandScore(dealerHandScore, dealerHand);
+    console.log(`The dealers score is now ${dealerScore}`);
+  }
+
+  playerScore = calculateHandScore(playerHandScore, playerHand);
+  logEndScore("dealer", dealerScore);
+  checkIfBust(playerScore, "player");
   findWinner();
 }
 
 startGame();
-function drawOneCard(e) {
+
+///////////// Game Functions
+
+/*function to draw one card, adds card to array 'firstTimeCards' checks if card exsits in 'firstTimeCards'already and if so
+draws a new card.*/
+function drawOneCard(whosDeck) {
   for (let i = 0; i < 1; i++) {
-    const cards = [
-      "Ace",
-      // 1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      "Jack",
-      "Queen",
-      "King",
-    ];
+    const cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
     const suit = ["Spades", "Hearts", "Diamonds", "Clubs"];
     let randomCard = cards[Math.floor(Math.random() * cards.length)];
-
     let randomSuit = suit[Math.floor(Math.random() * suit.length)];
-
     firstTimeCards.push(" " + randomCard + " Of " + randomSuit);
-    //    console.log(firstTimeCards);
     if (checkIfArrayIsUnique(firstTimeCards)) {
-      //   console.log("this is a new item in the array");
-      e.push(" " + randomCard + " Of " + randomSuit);
+      whosDeck.push(" " + randomCard + " Of " + randomSuit);
     } else {
-      //    console.log("I am popping off the last item in the array which is");
-      //     console.log(firstTimeCards[firstTimeCards.length - 1]);
       firstTimeCards.pop();
-      drawOneCard(e);
+      drawOneCard(whosDeck);
+    }
+    if (whosDeck.length > 2) {
+      console.log("The new card is " + randomCard + " Of " + randomSuit);
     }
   }
 }
 
+//Function for checking if the drawn cards are unique
 function checkIfArrayIsUnique(myArray) {
   return myArray.length === new Set(myArray).size;
 }
-/////calculate score of hand
 
+//function to check the score if the hand has an ace which would cause them to bust change the ace value to 1
 function calculateHandScore(score, whosHand) {
   for (let i = 0; i < whosHand.length; i++) {
     if (whosHand[i].includes("Ace")) {
@@ -111,30 +107,17 @@ function calculateHandScore(score, whosHand) {
       score += 10;
     }
   }
+  for (let i = 0; i < whosHand.length; i++) {
+    if (whosHand[i].includes("Ace") && score > 21) {
+      score -= 10;
+    }
+  }
   return score;
 }
 
-function drawAgain(score, whosHand, whosOldScore, newScore) {
-  if (score < 17) {
-    console.log("score is under 17");
-    console.log(`
-    
-drawing another card for the player`);
-    drawOneCard(whosHand);
-    newScore = calculateHandScore(whosOldScore, whosHand);
-    console.log(whosHand[whosHand.length - 1]);
-    console.log(newScore);
-  }
-  return newScore;
-}
-
-function checkIfBust(e, who) {
-  if (e > 21) {
-    console.log(`
-    
-The New Score for ${who} is ${e}`);
-
-    console.log(`${who} is bust `);
+function checkIfBust(score, who) {
+  if (score > 21) {
+    console.log(`${who} is bust with a score of ${score}`);
     return;
   }
   return;
@@ -142,14 +125,20 @@ The New Score for ${who} is ${e}`);
 
 function logEndScore(who, score) {
   if (score >= 17 && score <= 21) {
-    console.log(`${who} End Score = ${score}`);
+    console.log(`${who} sticks with a score of = ${score}`);
   }
 }
 
 function findWinner() {
-  if (playerScoreUpdated > dealerScore && playerScoreUpdated <= 21) {
-    console.log("The Player is the winner!");
+  if ((playerScore > dealerScore && playerScore <= 21) || dealerScore > 21) {
+    console.log(
+      `The Player is the winner! With a score of ${playerScore} while the dealer had a score of ${dealerScore}`
+    );
+  } else if (playerScore == dealerScore) {
+    console.log(
+      `The game ends in a draw player and dealer  had a score of ${playerScore}`
+    );
   } else {
-    console.log("The Dealer is the winner!");
+    console.log(`The Dealer is the winner! With a score of ${dealerScore}`);
   }
 }
